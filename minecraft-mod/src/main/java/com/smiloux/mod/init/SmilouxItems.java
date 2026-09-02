cd6f541b8c104cb4ac5c34748529b4ce77c9de52
smilouxgg-art <smilouxgg@gmail.com>
2026-09-01 21:05:36 -0400

    feat: register jukebox block item

--- minecraft-mod/src/main/java/com/smiloux/mod/init/SmilouxItems.java
+++ minecraft-mod/src/main/java/com/smiloux/mod/init/SmilouxItems.java
@@ -2,12 +2,19 @@ package com.smiloux.mod.init;
 
 import com.smiloux.mod.SmilouxMod;
 import com.smiloux.mod.item.SmilouxHeadphonesItem;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public final class SmilouxItems {
+    public static final Item SMILOUX_JUKEBOX = Registry.register(
+        Registries.ITEM,
+        Identifier.of(SmilouxMod.MOD_ID, "smiloux_jukebox"),
+        new BlockItem(SmilouxBlocks.SMILOUX_JUKEBOX, new Item.Settings())
+    );
     public static final Item HEADPHONES = Registry.register(
         Registries.ITEM,
         Identifier.of(SmilouxMod.MOD_ID, "smiloux_headphones"),
