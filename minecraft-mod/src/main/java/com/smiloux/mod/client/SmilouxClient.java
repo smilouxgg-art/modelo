d1637790e972c7fce4a162ee888b597188043822
smilouxgg-art <smilouxgg@gmail.com>
2026-09-01 21:04:38 -0400

    feat: register Smiloux client commands

--- minecraft-mod/src/main/java/com/smiloux/mod/client/SmilouxClient.java
+++ minecraft-mod/src/main/java/com/smiloux/mod/client/SmilouxClient.java
@@ -5,8 +5,8 @@ import com.smiloux.mod.client.discord.SmilouxDiscordRPC;
 import com.smiloux.mod.client.gui.MusicScreen;
 import com.smiloux.mod.client.gui.SmilouxHudEditScreen;
 import com.smiloux.mod.client.gui.SmilouxHudOverlay;
-import com.smiloux.mod.client.util.DependencyManager;
 import com.smiloux.mod.client.playlist.PlaylistManager;
+import com.smiloux.mod.client.util.DependencyManager;
 import com.smiloux.mod.network.SmilouxClientNetwork;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
@@ -14,7 +14,6 @@ import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public final class SmilouxClient implements ClientModInitializer {
@@ -26,8 +25,8 @@ public final class SmilouxClient implements ClientModInitializer {
     public void onInitializeClient() {
         OPEN_GUI = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.smiloux.open_gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M, "category.smiloux"));
         EDIT_HUD = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.smiloux.edit_hud", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_O, "category.smiloux"));
-
         SmilouxClientNetwork.register();
+        SmilouxCommands.register();
         PlaylistManager.ensureDirectory();
         DependencyManager.ensureInstalled();
         SmilouxDiscordRPC.initialize();
@@ -38,7 +37,6 @@ public final class SmilouxClient implements ClientModInitializer {
             ENGINE.tick(client);
             SmilouxDiscordRPC.tick(ENGINE.getCurrent());
         });
-
         HudRenderCallback.EVENT.register((drawContext, tickDelta) -> SmilouxHudOverlay.render(drawContext, ENGINE));
     }
 }
